5948ab740d8dc99d58296ca2e14761e08bea9902
Caio Galvão <caiogl1998@gmail.com>
2023-10-17 08:31:20 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/com/caiogl/workshopmongo/config/Instantiation.java
+++ src/main/java/com/caiogl/workshopmongo/config/Instantiation.java
@@ -13,18 +13,18 @@ import com.caiogl.workshopmongo.repository.UserRepository;
 public class Instantiation implements CommandLineRunner {
 
 	@Autowired
-	private UserRepository userRepository;
+	private UserRepository userReposiroty;
 	
 	@Override
-	public void run(String... args) throws Exception {
+	public void run(String... arg0) throws Exception {
 		
-	userRepository.deleteAll();	
+		userReposiroty.deleteAll();
 		
-	User maria = new User(null, "Maria Brown", "maria@gmail.com");
-	User alex = new User(null, "Alex Green", "alex@gmail.com");
-	User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
-	userRepository.saveAll(Arrays.asList(maria, alex, bob));
-	
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
 	}
+
 }
